56629dec04c074b24ea1037a56575364b2c1a52f
JC2Vergara <43939174+JC2Vergara@users.noreply.github.com>
2018-10-08 01:42:56 -0500

    Add files via upload

--- Servicios.php
+++ Servicios.php
@@ -1,6 +1,10 @@
 <?php 
 
-
-	require 'static/views/Servicios.views.php';
+	if ($g != ''  OR $g != 'servicios') {
+		
+		require 'static/views/Servicios.views.php';
+	}else{
+		header('Location:'.RUTA);
+	}
 
  ?>
